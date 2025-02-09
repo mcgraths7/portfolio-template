1748e0790050692166522bd159df06781a351fa1
Steven McGrath <mcgrath.steven7@gmail.com>
2025-02-09 11:44:53 -0500

    removed unused schema and type

--- types/sanity/copy.ts
+++
@@ -1,9 +0,0 @@
-import { PortableTextBlock } from "next-sanity";
-
-export type Copy = {
-  _id?: string;
-  _createdAt?: Date;
-  name: string;
-  slug: string;
-  content: PortableTextBlock[];
-};
