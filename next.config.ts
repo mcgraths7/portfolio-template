50bbf165bd815e4ad79f6f9bf443bdbdc333cb2f
Steven McGrath <mcgrath.steven7@gmail.com>
2025-02-09 15:52:22 -0500

    add prod sanity api endpoint

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: 'cdn.sanity.io',
         port: '',
       },
+      {
+        protocol: 'https',
+        hostname: `${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.apicdn.sanity.io`,
+        port: '',
+      }
     ],
   },
 }
